ee98abba757684f9b04cb6c238a0cc4defcc8148
Adrian31BT <adrian31baciliot@gmail.com>
2024-12-21 15:21:25 -0500

    Cambios en el inico

--- GestionBuses/App.js
+++ GestionBuses/App.js
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Inicio } from './app/screens/Inicio'
 
 export default function App() {
+  const StackPartidos = createNativeStackNavigator(); 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer >
+      <StackPartidos.Navigator initialRouteName='InicioNav'>
+        <StackPartidos.Screen name="InicioNav"  component={Inicio} options={{ title: 'Gestion Buses', headerTitleAlign: 'center'}}/>
+      </StackPartidos.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ GestionBuses/app/screens/Inicio.js
@@ -0,0 +1,46 @@
+import { StyleSheet, View } from 'react-native';
+import { Button, Text } from '@rneui/base'
+
+
+export const Inicio = ({navigation}) => {
+
+    return (
+        <View style={styles.container}>
+          <Text style={styles.bienvenida}>Bienvenidos</Text>
+          <Button
+            title='Lista de buses'
+            buttonStyle={styles.ListaBoton}
+            onPress={() => {
+                /* navigation.navigate("ListaPartidosNav", {usuario:usuario}); */
+            }}
+          />
+          <Button
+            title='Lista de rutas'
+            buttonStyle={styles.ListaBoton}
+            onPress={() => {
+                /* navigation.navigate("ListaPartidosNav", {usuario:usuario}); */
+            }}
+          />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    bienvenida: {
+      fontWeight: "bold",
+      fontSize: 45,
+    },
+    ListaBoton:{
+      borderRadius: 10,
+      marginTop: 10,
+      paddingHorizontal: 20,
+      backgroundColor:"#11a85b"
+    }
+  });
+  
